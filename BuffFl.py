1620d8438890bedac7ebc17c7ed2928d32c3f77b
TloiIos <tctloi123@gmail.com>
2025-05-03 22:18:24 +0700

    Update BuffFl.py

--- BuffFl.py
+++ BuffFl.py
@@ -1,5 +1,5 @@
 import requests, time
-username=input('Nhập Username Tik Tok ( Không Nhập "@") ')
+username=input('Nhập Username Tik Tok ( Không Nhập "@"): ')
     headers={'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7','accept-language': 'vi,fr-FR;q=0.9,fr;q=0.8,en-US;q=0.7,en;q=0.6','cache-control': 'max-age=0','priority': 'u=0, i','sec-ch-ua': '"Google Chrome";v="125", "Chromium";v="125", "Not.A/Brand";v="24"','sec-ch-ua-mobile': '?0','sec-fetch-dest': 'document','sec-fetch-mode': 'navigate','sec-fetch-site': 'cross-site','sec-fetch-user': '?1','upgrade-insecure-requests': '1','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',}
     access=requests.get('https://tikfollowers.com/free-tiktok-followers', headers=headers)
     try:
